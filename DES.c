7fe5a949ecabb9a217df07b9b9c8f4a23995a85b
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-05-04 19:33:29 +0200

    Ne znam sta se desava ovde

--- DES.c
+++ DES.c
@@ -3,10 +3,11 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+
 //testing...
 
 //Za sada ne radi :P
-int64_t IP(int64_t input)
+unsigned __int64 IP(unsigned __int64 input)
 {
     int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2, 60,\
                        52, 44, 36, 28, 20, 12, 4, 62, 54,\
@@ -15,7 +16,7 @@ int64_t IP(int64_t input)
                        25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,\
                        61, 53, 45, 37, 29, 21, 13, 5,\
                        63, 55, 47, 39, 31, 23, 15, 7}, i;
-    int64_t output = 0;
+    unsigned __int64 output = 0;
     for(i=0;i<64;i++)
     {
         if(input & (1<<(64-permutation[i])))
@@ -24,9 +25,21 @@ int64_t IP(int64_t input)
     return output;
 }
 
+void print_binary(unsigned __int64 input)
+{
+    int i;
+    for(i=0;i<64;i++)
+        if(input & (1<<(63-i)))
+            printf("1");
+        else
+            printf("0");
+    printf("\n");
+}
+
 int main()
 {
-    int64_t test = 7;
-    printf("%" PRId64 "\n", IP(test));
+    unsigned __int64 test = 8;
+    print_binary(test);
+    print_binary(IP(test));
     return 0;
 }
