37258be41480f3e0c633c9ca8f4cc163daccbcb9
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-05-17 00:22:23 +0200

    Revert "Probao bitfield"
    This reverts commit 5ecbb30f55242402c166042b6acc9841c999fd9a.

--- DES.c
+++ DES.c
@@ -7,12 +7,7 @@
 //testing...
 
 //Za sada ne radi :P
-typedef struct int64
-{
-    unsigned long long data : 64;
-}int64;
-
-int64 IP(int64 input)
+unsigned __int64 IP(unsigned __int64 input)
 {
     int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2, 60,\
                        52, 44, 36, 28, 20, 12, 4, 62, 54,\
@@ -21,21 +16,20 @@ int64 IP(int64 input)
                        25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,\
                        61, 53, 45, 37, 29, 21, 13, 5,\
                        63, 55, 47, 39, 31, 23, 15, 7}, i;
-    int64 output;
-    output.data = 0;
+    unsigned __int64 output = 0;
     for(i=0;i<64;i++)
     {
-        if(input.data & (1<<(64-permutation[i])))
-            output.data |= 1<<(63-i);
+        if(input & (1<<(64-permutation[i])))
+            output |= 1<<(63-i);
     }
     return output;
 }
 
-void print_binary(int64 input)
+void print_binary(unsigned __int64 input)
 {
     int i;
     for(i=0;i<64;i++)
-        if(input.data & (1<<(63-i)))
+        if(input & (1<<(63-i)))
             printf("1");
         else
             printf("0");
@@ -44,8 +38,7 @@ void print_binary(int64 input)
 
 int main()
 {
-    int64 test;
-    test.data = 8;
+    unsigned __int64 test = 8;
     print_binary(test);
     print_binary(IP(test));
     return 0;
