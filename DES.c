a4e95dba7164cbf1fd31eee36b3ac6167e4f8ebd
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-05-17 00:14:29 +0200

    Probao bitfield

--- DES.c
+++ DES.c
@@ -7,7 +7,12 @@
 //testing...
 
 //Za sada ne radi :P
-unsigned __int64 IP(unsigned __int64 input)
+typedef struct int64
+{
+    unsigned long long data : 64;
+}int64;
+
+int64 IP(int64 input)
 {
     int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2, 60,\
                        52, 44, 36, 28, 20, 12, 4, 62, 54,\
@@ -16,20 +21,21 @@ unsigned __int64 IP(unsigned __int64 input)
                        25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,\
                        61, 53, 45, 37, 29, 21, 13, 5,\
                        63, 55, 47, 39, 31, 23, 15, 7}, i;
-    unsigned __int64 output = 0;
+    int64 output;
+    output.data = 0;
     for(i=0;i<64;i++)
     {
-        if(input & (1<<(64-permutation[i])))
-            output |= 1<<(63-i);
+        if(input.data & (1<<(64-permutation[i])))
+            output.data |= 1<<(63-i);
     }
     return output;
 }
 
-void print_binary(unsigned __int64 input)
+void print_binary(int64 input)
 {
     int i;
     for(i=0;i<64;i++)
-        if(input & (1<<(63-i)))
+        if(input.data & (1<<(63-i)))
             printf("1");
         else
             printf("0");
@@ -38,7 +44,8 @@ void print_binary(unsigned __int64 input)
 
 int main()
 {
-    unsigned __int64 test = 8;
+    int64 test;
+    test.data = 8;
     print_binary(test);
     print_binary(IP(test));
     return 0;
