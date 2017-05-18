185f7cf841ed458ed19b040bd2ad80c764abfcc3
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-05-18 20:26:40 +0200

    Fixed initial permutation function

--- DES.c
+++ DES.c
@@ -1,52 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <inttypes.h>
+#include <string.h>
 
 
-//testing...
 
-//Za sada ne radi :P
-typedef struct int64
+char* IP(char* input)
 {
-    unsigned long long data : 64;
-}int64;
+    int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2,
+                         60, 52, 44, 36, 28, 20, 12, 4,
+                         62, 54, 46, 38, 30, 22, 14, 6,
+                         64, 56, 48, 40, 32, 24, 16, 8,
+                         57, 49, 41, 33, 25, 17,  9, 1,
+                         59, 51, 43, 35, 27, 19, 11, 3,
+                         61, 53, 45, 37, 29, 21, 13, 5,
+                         63, 55, 47, 39, 31, 23, 15, 7};
+    int i;
+    char* output = calloc(8, sizeof(char));
 
-int64 IP(int64 input)
-{
-    int permutation[] = {58, 50, 42, 34, 26, 18, 10, 2, 60,\
-                       52, 44, 36, 28, 20, 12, 4, 62, 54,\
-                       46, 38, 30, 22, 14, 6, 64, 56, 48,\
-                       40, 32, 24, 16, 8, 57, 49, 41, 33,\
-                       25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3,\
-                       61, 53, 45, 37, 29, 21, 13, 5,\
-                       63, 55, 47, 39, 31, 23, 15, 7}, i;
-    int64 output;
-    output.data = 0;
     for(i=0;i<64;i++)
     {
-        if(input.data & (1<<(64-permutation[i])))
-            output.data |= 1<<(63-i);
+		output[i/8] |= (((0x80 >> ((permutation[i] - 1)%8)) & input[(permutation[i] - 1)/8]) << ((permutation[i] - 1)%8)) >> i%8;
     }
     return output;
 }
 
-void print_binary(int64 input)
+void charToBin(char* input, int n)
 {
-    int i;
-    for(i=0;i<64;i++)
-        if(input.data & (1<<(63-i)))
-            printf("1");
-        else
-            printf("0");
+    int i,j;
+    for(i=0;i<n;i++)
+        for(j=0;j<8;j++)
+            if(input[i] & (1<<(7-j)))
+                printf("1");
+            else
+                printf("0");
     printf("\n");
 }
 
 int main()
 {
-    int64 test;
-    test.data = 8;
-    print_binary(test);
-    print_binary(IP(test));
+    char test[8] = {};
+    test[7] = 5;
+    charToBin(test, 8);
+    charToBin(IP(test), 8);
     return 0;
 }
