72fb44df25438575e648bd1bc88d8a1612bb47ad
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-05-18 20:42:30 +0200

    Added reverse permutation

--- DES.c
+++ DES.c
@@ -24,6 +24,26 @@ char* IP(char* input)
     return output;
 }
 
+char* reverseIP(char* input)
+{
+    int permutation[] =  {40,  8, 48, 16, 56, 24, 64, 32,
+                          39,  7, 47, 15, 55, 23, 63, 31,
+                          38,  6, 46, 14, 54, 22, 62, 30,
+                          37,  5, 45, 13, 53, 21, 61, 29,
+                          36,  4, 44, 12, 52, 20, 60, 28,
+                          35,  3, 43, 11, 51, 19, 59, 27,
+                          34,  2, 42, 10, 50, 18, 58, 26,
+                          33,  1, 41,  9, 49, 17, 57, 25};
+    int i;
+    char* output = calloc(8, sizeof(char));
+
+    for(i=0;i<64;i++)
+    {
+		output[i/8] |= (((0x80 >> ((permutation[i] - 1)%8)) & input[(permutation[i] - 1)/8]) << ((permutation[i] - 1)%8)) >> i%8;
+    }
+    return output;
+}
+
 void charToBin(char* input, int n)
 {
     int i,j;
@@ -38,9 +58,12 @@ void charToBin(char* input, int n)
 
 int main()
 {
-    char test[8] = {};
+    char *test = calloc(8, sizeof(char));
     test[7] = 5;
     charToBin(test, 8);
-    charToBin(IP(test), 8);
+    test = IP(test);
+    charToBin(test, 8);
+    test = reverseIP(test);
+    charToBin(test, 8);
     return 0;
 }
