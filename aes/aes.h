36ec415387390649be4a869a794aba8b6f08b1e4
Biza <kosta.bizetic@gmail.com>
2017-05-22 14:05:58 +0200

    Added key expansion

--- aes/aes.h
+++ aes/aes.h
@@ -1,6 +1,17 @@
 #ifndef AES_H
 #define AES_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TEST_DEF
+
+typedef unsigned char uc;
+
+#ifdef TEST_DEF
+
 void print(void);
+
+#endif // TEST_DEF
 
 #endif // AES_H
--- aes/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "aes.h"
-
-int main()
-{
-    print();
-    return 0;
-}
