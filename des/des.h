bd9f0dc2be38f9cf23f78da647b72f48d9ee2cb2
Luka Dojcilovic <l.dojcilovic@gmail.com>
2017-06-26 12:46:24 +0200

    Added doxygen comments.

--- des/des.h
+++ des/des.h
@@ -3,15 +3,78 @@
 
 #include "../global.h"
 
-int desDecryptFileCBC(char *name, uc* key);
-int desDecryptFileECB(char *name, uc* key);
+/**
+* @brief Funkcija za enkripciju fajla DES algoritmom u ECB modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
 int desEncryptFileCBC(char *name, uc* key);
+
+/**
+* @brief Funkcija za dekripciju fajla DES algoritmom u ECB modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
+int desDecryptFileECB(char *name, uc* key);
+
+/**
+* @brief Funkcija za dekripciju fajla DES algoritmom u CBC modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
 int desEncryptFileECB(char *name, uc* key);
 
-int tdesDecryptFileCBC(char *name, uc* key1, uc* key2, uc* key3);
-int tdesDecryptFileECB(char *name, uc* key1, uc* key2, uc* key3);
-int tdesEncryptFileCBC(char *name, uc* key1, uc* key2, uc* key3);
+/**
+* @brief Funkcija za enkripciju fajla DES algoritmom u CBC modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
+int desDecryptFileCBC(char *name, uc* key);
+
+
+/**
+* @brief Funkcija za enkripciju fajla tDES algoritmom u ECB modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
 int tdesEncryptFileECB(char *name, uc* key1, uc* key2, uc* key3);
+
+/**
+* @brief Funkcija za dekripciju fajla tDES algoritmom u ECB modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
+int tdesDecryptFileECB(char *name, uc* key1, uc* key2, uc* key3);
+
+/**
+* @brief Funkcija za enkripciju fajla tDES algoritmom u CBC modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
+int tdesEncryptFileCBC(char *name, uc* key1, uc* key2, uc* key3);
+
+/**
+* @brief Funkcija za dekripciju fajla tDES algoritmom u CBC modu.
+* @param[in] name Path fajla koji treba dekriptovati.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @param[in] key1 58-bitni kljuc za dekripciju.
+* @return Greske
+*/
+int tdesDecryptFileCBC(char *name, uc* key1, uc* key2, uc* key3);
 
 #endif // _DES_H_
 
